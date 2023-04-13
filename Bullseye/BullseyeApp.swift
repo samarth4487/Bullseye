69cddec55f3c7ba4300694171f3087c81390c89b
Samarth Paboowal <samarthpaboowal@yahoo.co.in>
2023-04-13 22:18:29 +0530

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Samarth Paboowal on 13/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
